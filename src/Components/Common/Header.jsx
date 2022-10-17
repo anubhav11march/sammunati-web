8ecd62631221c9270d258af47168f911d82a4bda
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-10-17 19:13:01 +0600

    small charecter chnage bug fiixed

--- src/Components/Common/Header.jsx
+++ src/Components/Common/Header.jsx
@@ -1,183 +1,201 @@
-import React, { useState, useEffect } from "react";
-import "../../Assets/Css/header.css";
-import { json, useNavigate } from "react-router-dom";
-import logo from "../../Assets/Images/logo.png";
 import NotificationsIcon from "@mui/icons-material/Notifications";
-import person from "../../Assets/Images/men.png";
-import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import Popper from "@mui/material/Popper";
-import Fade from "@mui/material/Fade";
-import NotificationCard from "./NotificationCard";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Button } from "@mui/material";
 import { IconButton } from "@mui/material";
+import Fade from "@mui/material/Fade";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Popper from "@mui/material/Popper";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import "../../Assets/Css/header.css";
+import logo from "../../Assets/Images/logo.png";
+import person from "../../Assets/Images/men.png";
 import { GetUser } from "../Api/Api";
 import useAuth from "../hooks/useAuth";
-import { useTranslation } from "react-i18next";
-import i18n from "i18next";
+import NotificationCard from "./NotificationCard";
 
 function Header({ langChange }) {
-  const { auth, user } = useAuth();
-  const [isLoggedIn, setisLoggedIn] = useState(false);
-  const [loginData, setloginData] = useState();
-  const navigate = useNavigate();
-  const { t, i18n } = useTranslation(["main"]);
-  // notification
-  const [open, setOpen] = React.useState(false);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [placement, setPlacement] = React.useState();
+    const { auth, user } = useAuth();
+    const [isLoggedIn, setisLoggedIn] = useState(false);
+    const [loginData, setloginData] = useState();
+    const navigate = useNavigate();
+    const { t, i18n } = useTranslation(["main"]);
+    // notification
+    const [open, setOpen] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [placement, setPlacement] = React.useState();
 
-  const handleClick = (newPlacement) => (event) => {
-    setAnchorEl(event.currentTarget);
-    setOpen((prev) => placement !== newPlacement || !prev);
-    setPlacement(newPlacement);
-  };
+    const handleClick = (newPlacement) => (event) => {
+        setAnchorEl(event.currentTarget);
+        setOpen((prev) => placement !== newPlacement || !prev);
+        setPlacement(newPlacement);
+    };
 
-  useEffect(() => {
-    if (auth) {
-      setisLoggedIn(true);
-      const calldata = async () => {
-        try {
-          const data = await GetUser();
-          setloginData(data?.data?.data);
-        } catch (error) {
-          console.log(error);
+    useEffect(() => {
+        if (auth) {
+            setisLoggedIn(true);
+            const calldata = async () => {
+                try {
+                    const data = await GetUser();
+                    setloginData(data?.data?.data);
+                } catch (error) {
+                    console.log(error);
+                }
+            };
+            calldata();
+        } else {
+            setisLoggedIn(false);
         }
-      };
-      calldata();
-    } else {
-      setisLoggedIn(false);
-    }
-  }, []);
+    }, []);
 
-  useEffect(() => {
-    if (localStorage.getItem("i18nextLng")?.length > 2) {
-      i18n.changeLanguage("en");
-    }
-  }, []);
+    useEffect(() => {
+        if (localStorage.getItem("i18nextLng")?.length > 2) {
+            i18n.changeLanguage("en");
+        }
+    }, []);
 
-  const handleLanguage = (e) => {
-    i18n.changeLanguage(e.target.value);
-  };
+    const handleLanguage = (e) => {
+        i18n.changeLanguage(e.target.value);
+    };
 
-  //popper
-  const [anchorEl2, setanchorEl2] = React.useState(null);
-  const open2 = Boolean(anchorEl2);
-  const handleClick2 = (event) => {
-    setanchorEl2(event.currentTarget);
-  };
-  const handleClose = () => {
-    setanchorEl2(null);
-  };
+    //popper
+    const [anchorEl2, setanchorEl2] = React.useState(null);
+    const open2 = Boolean(anchorEl2);
+    const handleClick2 = (event) => {
+        setanchorEl2(event.currentTarget);
+    };
+    const handleClose = () => {
+        setanchorEl2(null);
+    };
 
-  const handleLogout = () => {
-    handleClose();
-    localStorage.removeItem("token");
-    localStorage.removeItem("accessToken");
-    setisLoggedIn(false);
-    window.location.reload();
-  };
+    const handleLogout = () => {
+        handleClose();
+        localStorage.removeItem("token");
+        localStorage.removeItem("accessToken");
+        setisLoggedIn(false);
+        window.location.reload();
+    };
 
-  return (
-    <>
-      <header className="header-wrapper mb-3">
-        <nav className="header-contet  py-2">
-          <div className="container-fluid d-flex">
-            <div className="col-8 d-flex logo-container">
-              <img src={logo} alt="logo" onClick={() => navigate("/")} />
-              <form className="d-flex w-50 ms-4" role="search">
-                <input
-                  className="form-control me-2 "
-                  type="search"
-                  placeholder={`${t("Search all videos")}`}
-                  aria-label="Search"
-                />
-              </form>
-            </div>
+    return (
+        <>
+            <header className="header-wrapper mb-3">
+                <nav className="header-contet  py-2">
+                    <div className="container-fluid d-flex">
+                        <div className="col-8 d-flex logo-container">
+                            <img
+                                src={logo}
+                                alt="logo"
+                                onClick={() => navigate("/")}
+                            />
+                            <form className="d-flex w-50 ms-4" role="search">
+                                <input
+                                    className="form-control me-2 "
+                                    type="search"
+                                    placeholder={`${t("Search all videos")}`}
+                                    aria-label="Search"
+                                />
+                            </form>
+                        </div>
 
-            <div className="col-4 d-flex justify-content-end">
-              <div className="dropdown mx-3">
-                <select
-                  className="form-select w-100  form-select-md "
-                  onChange={handleLanguage}
-                  aria-label=".form-select-lg example"
-                  value={localStorage.getItem("i18nextLng")}
-                >
-                  <option value="en">English</option>
-                  <option value="hi">हिन्दी</option>
-                  {/* <option value="marathi">मराठी</option>
+                        <div className="col-4 d-flex justify-content-end">
+                            <div className="dropdown mx-3">
+                                <select
+                                    className="form-select w-100  form-select-md "
+                                    onChange={handleLanguage}
+                                    aria-label=".form-select-lg example"
+                                    value={localStorage.getItem("i18nextLng")}
+                                >
+                                    <option value="en">English</option>
+                                    <option value="hi">हिन्दी</option>
+                                    {/* <option value="marathi">मराठी</option>
                   <option value="bandi">బండి</option>
                   <option value="tamil">தமிழ்</option>
                   <option value="arabic">عربى</option>
                   <option value="urdu">اردو</option> */}
-                </select>
-              </div>
+                                </select>
+                            </div>
 
-              {isLoggedIn ? (
-                <div className="d-flex align-items-center afterlogin">
-                  <NotificationsIcon onClick={handleClick("bottom-end")} s />
-                  <h5>{loginData?.name}</h5>
-                  <IconButton
-                    id="basic-button"
-                    aria-controls={open2 ? "basic-menu" : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open2 ? "true" : undefined}
-                    onClick={handleClick2}
-                  >
-                    <img src={person} alt="noimg" />
-                  </IconButton>
-                  <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl2}
-                    open={open2}
-                    onClose={handleClose}
-                    MenuListProps={{
-                      "aria-labelledby": "basic-button",
-                    }}
-                  >
-                    <MenuItem onClick={() => navigate("/editprofile")}>
-                    {t("Profile")}  
-                    </MenuItem>
-                    <MenuItem onClick={() => navigate("/changepassword")}>
-                    {t("Change Password")} 
-                    </MenuItem>
-                    <MenuItem onClick={handleLogout}> {t("Logout")}  </MenuItem>
-                  </Menu>
-                </div>
-              ) : (
-                <button
-                  onClick={() => navigate("/login")}
-                  className="btn  btn-outline-secondary mx-3"
-                >
-                  {t("signin")}
-                </button>
-              )}
-            </div>
-          </div>
-        </nav>
-      </header>
-      <Popper
-        open={open}
-        placement={placement}
-        anchorEl={anchorEl}
-        transition
-        style={{ zIndex: "10000", marginTop: "15px" }}
-      >
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={350}>
-            <div className="popper-Box">
-              <div className="d-flex flex-column py-2">
-                <NotificationCard />
-                <NotificationCard />
-              </div>
-            </div>
-          </Fade>
-        )}
-      </Popper>
-    </>
-  );
+                            {isLoggedIn ? (
+                                <div className="d-flex align-items-center afterlogin">
+                                    <NotificationsIcon
+                                        onClick={handleClick("bottom-end")}
+                                        s
+                                    />
+                                    <h5>Hi, {loginData?.name}</h5>
+                                    <IconButton
+                                        id="basic-button"
+                                        aria-controls={
+                                            open2 ? "basic-menu" : undefined
+                                        }
+                                        aria-haspopup="true"
+                                        aria-expanded={
+                                            open2 ? "true" : undefined
+                                        }
+                                        onClick={handleClick2}
+                                    >
+                                        <img src={person} alt="noimg" />
+                                    </IconButton>
+                                    <Menu
+                                        id="basic-menu"
+                                        anchorEl={anchorEl2}
+                                        open={open2}
+                                        onClose={handleClose}
+                                        MenuListProps={{
+                                            "aria-labelledby": "basic-button",
+                                        }}
+                                    >
+                                        <MenuItem
+                                            onClick={() =>
+                                                navigate("/editprofile")
+                                            }
+                                        >
+                                            {t("Profile")}
+                                        </MenuItem>
+                                        <MenuItem
+                                            onClick={() =>
+                                                navigate("/changepassword")
+                                            }
+                                        >
+                                            {t("Change Password")}
+                                        </MenuItem>
+                                        <MenuItem onClick={handleLogout}>
+                                            {" "}
+                                            {t("Logout")}{" "}
+                                        </MenuItem>
+                                    </Menu>
+                                </div>
+                            ) : (
+                                <button
+                                    onClick={() => navigate("/login")}
+                                    className="btn  btn-outline-secondary mx-3"
+                                >
+                                    {t("signin")}
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <Popper
+                open={open}
+                placement={placement}
+                anchorEl={anchorEl}
+                transition
+                style={{ zIndex: "10000", marginTop: "15px" }}
+            >
+                {({ TransitionProps }) => (
+                    <Fade {...TransitionProps} timeout={350}>
+                        <div className="popper-Box">
+                            <div className="d-flex flex-column py-2">
+                                <NotificationCard />
+                                <NotificationCard />
+                            </div>
+                        </div>
+                    </Fade>
+                )}
+            </Popper>
+        </>
+    );
 }
 
 export default Header;
--- src/Components/Common/Sidebar.jsx
+++ src/Components/Common/Sidebar.jsx
@@ -1,70 +1,158 @@
-import React from 'react'
-import '../../Assets/Css/sidebar.css'
-import VideocamIcon from '@mui/icons-material/Videocam';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import ListIcon from '@mui/icons-material/List';
-import ArticleIcon from '@mui/icons-material/Article';
-import ContactSupportIcon from '@mui/icons-material/ContactSupport';
-import PhoneIcon from '@mui/icons-material/Phone';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import ArticleIcon from "@mui/icons-material/Article";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import ListIcon from "@mui/icons-material/List";
+import PhoneIcon from "@mui/icons-material/Phone";
+import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
+import "../../Assets/Css/sidebar.css";
 
 function Sidebar() {
     const { t } = useTranslation(["main"]);
     const location = useLocation();
-    
+
     return (
         <>
             <aside className="col-1 sidebar-wrapper mx-3">
                 <div className="sidebar-content">
-                    
-                   {/* video buttons  */}
+                    {/* video buttons  */}
 
                     <nav className="sidebar-content-nav">
-                        <Link to={'/'} className={`sidebar-content-links ${location.pathname ==="/" ? ("active-block"):(null)} `}>
-                            <VideocamIcon className={`${location.pathname ==="/" ? ("active-icon"):(null)}`}/>
+                        <Link
+                            to={"/"}
+                            className={`sidebar-content-links ${
+                                location.pathname === "/"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <VideocamIcon
+                                className={`${
+                                    location.pathname === "/"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
                             <h6>{t("home")}</h6>
                         </Link>
 
-                        <Link to={'/playlist'} className={`sidebar-content-links ${location.pathname ==="/playlist" ? ("active-block"):(null)} `}>
-                            <SubscriptionsIcon className={`${location.pathname.includes("/playlist") ? ("active-icon"):(null)}`}  />
+                        <Link
+                            to={"/playlist"}
+                            className={`sidebar-content-links ${
+                                location.pathname === "/playlist"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <SubscriptionsIcon
+                                className={`${
+                                    location.pathname.includes("/playlist")
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
                             <h6>{t("playlist")}</h6>
                         </Link>
 
-                        <Link to={'/categories'} className={`sidebar-content-links ${location.pathname ==="/categories" ? ("active-block"):(null)} `}>
-                            <ListIcon className={`${location.pathname ==="/categories" ? ("active-icon"):(null)}`}/>
+                        <Link
+                            to={"/categories"}
+                            className={`sidebar-content-links ${
+                                location.pathname === "/categories"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <ListIcon
+                                className={`${
+                                    location.pathname === "/categories"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
                             <h6>{t("categories")}</h6>
                         </Link>
 
-                        <Link to={'/blogs'} className={`sidebar-content-links ${location.pathname ==="/blogs" ? ("active-block"):(null)} `}>
-                            <ArticleIcon className={`${location.pathname ==="/blogs" ? ("active-icon"):(null)}`}/>
-                            <h6>{t('Blogs')}</h6>
+                        <Link
+                            to={"/blogs"}
+                            className={`sidebar-content-links ${
+                                location.pathname === "/blogs"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <ArticleIcon
+                                className={`${
+                                    location.pathname === "/blogs"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
+                            <h6>{t("Blogs")}</h6>
                         </Link>
                     </nav>
 
                     {/* query buttons */}
 
-                    <nav className='sidebar-content-help-nav'>
-                        <Link to={'/query'} className={`sidebar-content-help-links ${location.pathname ==="/query" ? ("active-block"):(null)} `}>
-                            <QuestionAnswerIcon className={`${location.pathname ==="/query" ? ("active-icon"):(null)}`}/>
-                            <h6>{t('Query')}</h6>
+                    <nav className="sidebar-content-help-nav">
+                        <Link
+                            to={"/query"}
+                            className={`sidebar-content-help-links ${
+                                location.pathname === "/query"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <QuestionAnswerIcon
+                                className={`${
+                                    location.pathname === "/query"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
+                            <h6>{t("Query")}</h6>
                         </Link>
-                        <Link to={'/faq'} className={`sidebar-content-help-links ${location.pathname ==="/faq" ? ("active-block"):(null)} `}>
-                            <ContactSupportIcon className={`${location.pathname ==="/faq" ? ("active-icon"):(null)}`}/>
-                            <h6 className='text-center'>{t('Faq')}</h6>
+                        <Link
+                            to={"/faq"}
+                            className={`sidebar-content-help-links ${
+                                location.pathname === "/faq"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <ContactSupportIcon
+                                className={`${
+                                    location.pathname === "/faq"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
+                            <h6 className="text-center">{t("Faq")}</h6>
                         </Link>
-                        <Link to={'/contactus'} className={`sidebar-content-help-links ${location.pathname ==="/contactus" ? ("active-block"):(null)} `}>
-                            <PhoneIcon className={`${location.pathname ==="/contactus" ? ("active-icon"):(null)}`}/>
-                            <h6>{t('Contact us')}</h6>
+                        <Link
+                            to={"/contactus"}
+                            className={`sidebar-content-help-links ${
+                                location.pathname === "/contactus"
+                                    ? "active-block"
+                                    : null
+                            } `}
+                        >
+                            <PhoneIcon
+                                className={`${
+                                    location.pathname === "/contactus"
+                                        ? "active-icon"
+                                        : null
+                                }`}
+                            />
+                            <h6>{t("Contact us")}</h6>
                         </Link>
                     </nav>
-
                 </div>
             </aside>
         </>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
